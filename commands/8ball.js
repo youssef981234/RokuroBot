22f848709660f12bcc0d220ef290ad6b0e98172a
ClumsyYT <33505621+ClumsyYT@users.noreply.github.com>
2017-11-10 13:59:32 -0600

    Update 8ball.js

--- commands/8ball.js
+++ commands/8ball.js
@@ -1,5 +1,11 @@
 const Discord = require('discord.js');
 
+var fourtunes = {
+	"Yes",
+	"No",
+	"It's up to you man"
+};
+
 module.exports.run = async (bot, message, args) => {
 	if (args[1]) message.channel.send(fortunes[Math.floor(Math.random() * fortunes.length)]);
 	else message.channel.send("I don't know");
